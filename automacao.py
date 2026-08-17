98fc58092ee7c1d327269879000ee961d32d900b
mateusmarcelino23 <mateusmarcelinodev@gmail.com>
2026-08-17 17:58:15 -0300

    Fazendo a automação funcionar para área de trabalho em inglês e em português

--- automacao.py
+++ automacao.py
@@ -3,23 +3,29 @@ import shutil # o shutil ajuda a mover arquivos e pastas com segurança
 
 pasta_home = Path.home() # cria variável da pasta principal da área de trabalho
 
+
+def idiomas_pasta(nome_en, nome_pt): # função para lidar com idiomas diferentes do sistema
+    caminho_en = pasta_home / nome_en
+    caminho_pt = pasta_home / nome_pt
+    return caminho_en if caminho_en.exists() else caminho_pt # se o o idioma das pastas não estiverem em inglês, analisar em português
+
+pasta_docs = idiomas_pasta("Documents", "Documentos")
+pasta_pics = idiomas_pasta("Pictures", "Imagens")
+pasta_vids = idiomas_pasta("Videos", "Vídeos")
+pasta_mus = idiomas_pasta("Music", "Música")
+pasta_down = idiomas_pasta("Downloads", "Downloads") # usando a função para encontrar o nome das pastas de acordo com o idioma
+
 ORGANIZACAO = { # dicionário para listar a organização das pastas de acordo com os sufixos dos arquivos
-    ".txt": pasta_home / "Documents",
-    ".pdf": pasta_home / "Documents",
-    ".docx": pasta_home / "Documents",
-    ".jpg": pasta_home / "Pictures",
-    ".png": pasta_home / "Pictures",
-    ".mp4": pasta_home / "Videos",
-    ".mp3": pasta_home / "Music"
+    ".txt": pasta_docs,
+    ".pdf": pasta_docs,
+    ".docx": pasta_docs,
+    ".jpg": pasta_pics,
+    ".png": pasta_pics,
+    ".mp4": pasta_vids,
+    ".mp3": pasta_mus
 }
 
-pastas_para_organizar = { # dicionário com as 5 pastas onde o script vai entrar para procurar arquivos perdidos
-    pasta_home / "Documents",
-    pasta_home / "Music",
-    pasta_home / "Pictures",
-    pasta_home / "Videos",
-    pasta_home / "Downloads",
-}
+pastas_para_organizar = [pasta_docs, pasta_pics, pasta_vids, pasta_mus, pasta_down] # lista de pastas a serem analisadas
 
 for pasta_atual in pastas_para_organizar: # Pega todas as pastas para realizar o script, uma por uma
     if not pasta_atual.exists(): 
